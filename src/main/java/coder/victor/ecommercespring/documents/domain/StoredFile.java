169142fc8e9cf91a50bbbd0eb36baa14c8768432
VictorM-Coder <martinsvictor6781@gmail.com>
2026-07-15 14:03:38 -0300

    feat: implement has url

--- src/main/java/coder/victor/ecommercespring/documents/domain/StoredFile.java
+++ src/main/java/coder/victor/ecommercespring/documents/domain/StoredFile.java
@@ -2,6 +2,7 @@ package coder.victor.ecommercespring.documents.domain;
 
 import coder.victor.ecommercespring.config.exceptions.ApiException;
 import coder.victor.ecommercespring.documents.exceptions.DocumentsErrorCode;
+import coder.victor.ecommercespring.models.User;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -24,16 +25,33 @@ public class StoredFile {
     @Column(name = "updated_at")
     private Instant updatedAt;
 
+    @ManyToOne
+    @JoinColumn(name = "created_by_id")
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "last_update_by_id")
+    private User lastUpdateBy;
+
     private String url;
 
     public String getUrl() {
         return url;
     }
 
-    public void updateUrl(String url) {
+    public void updateUrl(String url, User user) {
         if (url == null || url.trim().isEmpty()) {
             throw new ApiException(DocumentsErrorCode.DOCUMENT_URL_CANNOT_BE_NULL_OR_EMPTY);
         }
         this.url = url;
+        this.lastUpdateBy = user;
+    }
+
+    public boolean hasUrl() {
+        return url != null && !url.trim().isEmpty();
+    }
+
+    public User getLastUptadeBy() {
+        return lastUpdateBy;
     }
 }
--- src/test/java/coder/victor/ecommercespring/documents/domain/StoredFileTest.java
+++ src/test/java/coder/victor/ecommercespring/documents/domain/StoredFileTest.java
@@ -2,13 +2,18 @@ package coder.victor.ecommercespring.documents.domain;
 
 import coder.victor.ecommercespring.config.exceptions.ApiException;
 import coder.victor.ecommercespring.documents.exceptions.DocumentsErrorCode;
+import coder.victor.ecommercespring.models.User;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StoredFileTest {
+
+    public static final User USER_MOCKED = Mockito.mock(User.class);
+    public static final String VALID_URL = "http://example.com/file.txt";
 
     @Test
     @DisplayName("Test updateUrl should throw ApiException when url is null")
@@ -17,7 +22,7 @@ public class StoredFileTest {
 
         ApiException apiException = Assertions.assertThrows(
                 ApiException.class,
-                () -> storedFile.updateUrl(null)
+                () -> storedFile.updateUrl(null, USER_MOCKED)
         );
 
         assertEquals(DocumentsErrorCode.DOCUMENT_URL_CANNOT_BE_NULL_OR_EMPTY, apiException.getErrorCodeInterface());
@@ -30,9 +35,36 @@ public class StoredFileTest {
 
         ApiException apiException = Assertions.assertThrows(
                         ApiException.class,
-                        () -> storedFile.updateUrl(" ")
+                        () -> storedFile.updateUrl(" ", USER_MOCKED)
         );
 
         assertEquals(DocumentsErrorCode.DOCUMENT_URL_CANNOT_BE_NULL_OR_EMPTY, apiException.getErrorCodeInterface());
+    }
+
+    @DisplayName("Test updateUrl should update url and lastUpdateBy when url is valid")
+    @Test
+    public void testUpdateUrlWithValidUrl() {
+        StoredFile storedFile = new StoredFile();
+
+        storedFile.updateUrl(VALID_URL, USER_MOCKED);
+
+        assertTrue(storedFile.hasUrl());
+        assertEquals(USER_MOCKED, storedFile.getLastUptadeBy());
+        assertEquals(VALID_URL, storedFile.getUrl());
+    }
+
+    @DisplayName("Test hasUrl should return false when url is null or empty")
+    @Test
+    public void testHasUrlWithNullOrEmpty() {
+        StoredFile storedFile = new StoredFile();
+        assertFalse(storedFile.hasUrl());
+    }
+
+    @DisplayName("Test hasUrl should return true when url is not null or empty")
+    @Test
+    public void testHasUrlWithNotNullOrEmpty() {
+        StoredFile storedFile = new StoredFile();
+        storedFile.updateUrl(VALID_URL, USER_MOCKED);
+        assertTrue(storedFile.hasUrl());
     }
 }
